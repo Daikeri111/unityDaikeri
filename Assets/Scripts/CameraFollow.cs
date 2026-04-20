de89facfdc04766513be0921de152bdb38ac2fcf
unknown <kirasem33@gmail.com>
2026-04-20 17:22:09 +0300

    start

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;
+    public float smoothSpeed = 0.125f;
+    public Vector3 offset;
+    private void LateUpdate()
+    {
+        Vector3 desiredPosition = target.position + offset;
+        transform.position = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+        transform.LookAt(target);
+    }
+}
